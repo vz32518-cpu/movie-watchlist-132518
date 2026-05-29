2e8141f9039172f6ac5ad2c66fefd8d12d7d1e03
vz32518-cpu <vz32518@seeu.edu.mk>
2026-05-29 19:09:13 +0200

    Implemented login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,15 +1,33 @@
-// TODO: import and use AuthContext to call login() when the button is clicked
-// TODO: after login, navigate to /movies
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
-  return (
-    <div className="p-8 max-w-sm mx-auto">
-      <h1 className="text-2xl font-bold text-gray-900 mb-6">Login</h1>
-      <button className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
-        Login
-      </button>
-    </div>
-  )
+    const { login } = useContext(AuthContext);
+
+    const navigate = useNavigate();
+
+    const handleLogin = () => {
+        login();
+        navigate("/movies");
+    };
+
+    return (
+        <div className="p-8 max-w-sm mx-auto">
+
+            <h1 className="text-2xl font-bold text-gray-900 mb-6">
+                Login
+            </h1>
+
+            <button
+                onClick={handleLogin}
+                className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
+            >
+                Login
+            </button>
+
+        </div>
+    );
 }
 
-export default LoginPage
+export default LoginPage;
